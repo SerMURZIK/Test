d64e0d4c6813f53d26bf48c1cda7fee262b2ed1b
SerMURZIK <118277030+SerMURZIK@users.noreply.github.com>
2023-02-26 18:49:22 +0400

    changed formalization

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,28 +1,29 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 import java.util.List;
 
-class Test {
+class TestMain {
 
     Main main;
 
     @BeforeEach
-    void beforeEach() {
+    public void beforeEach() {
         main = new Main();
     }
 
-    @org.junit.jupiter.api.Test
-    void add() {
+    @Test
+    public void add() {
         int a = main.getProductsSize();
         main.add("Хлеб");
         int b = main.getProductsSize();
         Assertions.assertEquals(a, b - 1);
     }
 
-    @org.junit.jupiter.api.Test
-    void delete() {
+    @Test
+    public void delete() {
         main.add("Хлеб");
         int a = main.getProductsSize();
         main.delete("Хлеб");
@@ -30,8 +31,8 @@ class Test {
         Assertions.assertEquals(a, b + 1);
     }
 
-    @org.junit.jupiter.api.Test
-    void show() {
+    @Test
+    public void show() {
         List productsExpected = main.getProducts();
         List productsActual = new LinkedList();
         for (int i = 0;  i < productsExpected.size(); i++) {
